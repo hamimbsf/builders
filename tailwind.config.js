e0c7cc5018cf64ca4fe5efa422c29851fbd1549a
hamimbsf <hamim.bsf@gmail.com>
2024-08-22 17:26:19 +0600

    added challenge section

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/img/hero-pattern.svg')",
+          'footer-texture': "url('/img/footer-texture.png')",
+          'bg-texture': "url('/assets/bg.jpg')",
+        }
+      }
+    }
+  }
